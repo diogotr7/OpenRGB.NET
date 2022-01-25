d22c95187ff44b6d784638151b5ee740623959ac
Benny Mackney <bmackneypublic@gmail.com>
2022-01-25 18:19:25 +1100

    Store owning client in Device, Zone, add Update convenience method

--- OpenRGB.NET/OpenRGBClient.cs
+++ OpenRGB.NET/OpenRGBClient.cs
@@ -163,7 +163,7 @@ namespace OpenRGB.NET
                 throw new ArgumentException(nameof(id));
 
             SendMessage(CommandId.RequestControllerData, BitConverter.GetBytes(ProtocolVersion), (uint)id);
-            return Device.Decode(ReadMessage(), ProtocolVersion, id);
+            return Device.Decode(ReadMessage(), ProtocolVersion, this, id);
         }
 
         /// <inheritdoc/>
